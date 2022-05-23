efd2e81cd724c8b68048acde6360eda99723e060
Sandra Sanders <sandy-sanders@msn.com>
2022-05-22 21:56:52 -0700

    setup

--- discover-coffe-stores/pages/_app.js
+++ discover-coffe-stores/pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />{ " "}
+      <footer>
+        <p>@ 2022 Sanders</p>
+      </footer>
+    </div>
+  );
 }
 
 export default MyApp
