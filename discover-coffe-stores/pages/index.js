bd596a5c88add1c767bfd745fa07b2989e10a388
Sandra Sanders <sandy-sanders@msn.com>
2022-06-03 14:57:08 -0700

    card section

--- discover-coffe-stores/pages/index.js
+++ discover-coffe-stores/pages/index.js
@@ -2,9 +2,9 @@ import Head from 'next/head';
 import Image from "next/image";
 import styles from '../styles/Home.module.css'
 import Banner from '../components/banner';
+import Card from '../components/card';
 
-
-export default function Home() {
+export default function Home() { 
   const handleOnBannerBtnClick = () => {
     console.log('hi banner button');
   }
@@ -24,10 +24,13 @@ export default function Home() {
           <Image src="/static/hero-image.png"
             width={700}
             height={400}
-            alt="hero image"
-         
-          />
+            alt="hero image"/>
         </div>
+        <Card
+        name="DarkHorse Coffee"
+        imgUrl="/static/hero-image.png"
+        href="/coffee-store/darkhorse-coffee"
+        />
       </main>
     </div>
   
